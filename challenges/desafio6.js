c6953ae9480f7e86dcbf439859ccd5f626c3101d
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-17 15:01:49 -0300

    [requisito 6 ] lint

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,2 +1,2 @@
 db.voos.find({ vooId: 756807 },
-{ "empresa.sigla": true, "empresa.nome": true, _id: false,  passageiros: true });
+{ "empresa.sigla": true, "empresa.nome": true, _id: false, passageiros: true });
