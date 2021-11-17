8041d42cbded50b4464281ff2d2844ba8779fbb2
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-17 17:13:26 -0300

    [requisito 27 ] resolucao

---
+++ challenges/desafio27.js
@@ -0,0 +1,10 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: 
+    db.voos.find({ $and: [
+  { natureza: { $eq: "Doméstica" } },
+  { "empresa.nome": { $eq : "PASSAREDO" }  }
+] } ).count()  
+});
+
+db.resumoVoos.find({ empresa: /passaredo/i }, {_id: false});
