cc6b8db9e9d07105fc465482fa1aa266609b0f9a
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-17 15:24:16 -0300

    [requisito 12 e 13 ] lint

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,1 @@
-db.voos.find({ "aeroportoDestino.pais": { $in: ["BRASIL", "ARGENTINA", "CHILE" ] } }).count();
+db.voos.find({ "aeroportoDestino.pais": { $in: ["BRASIL", "ARGENTINA", "CHILE"] } }).count();
--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,1 +1,1 @@
-db.voos.find({ "aeroportoDestino.continente": { $not: { $in: ["EUROPA", "ÁSIA", "OCEANIA" ] } } }).count();
+db.voos.find({ "aeroportoDestino.continente": { $not: { $in: ["EUROPA", "ÁSIA", "OCEANIA"] } } }).count();
