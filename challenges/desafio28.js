e3f0fc9078666549b2d24cb361d91a836b7fb9df
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-17 17:15:20 -0300

    [requisito 28 ] resolucao

---
+++ challenges/desafio28.js
@@ -0,0 +1,10 @@
+db.resumoVoos.insertOne({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: 
+    db.voos.find({ $and: [
+  { natureza: { $eq: "Doméstica" } },
+  { "empresa.nome": { $eq : "LATAM AIRLINES BRASIL" }  }
+] } ).count()  
+});
+
+db.resumoVoos.find({ empresa: /latam airlines brasil/i }, {_id: false});
