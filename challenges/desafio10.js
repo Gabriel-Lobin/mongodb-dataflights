3371879ee993497e4b190d36da76da8a7c1895e5
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-17 15:14:31 -0300

    [requisito 10 ] lint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,10 @@
-db.voos.find({ "empresa.nome": "GOL", ano: 2017 }, { vooId: true, "empresa.nome": true, "aeroportoOrigem.nome": true, "aeroportoDestino.nome": true, mes: true, ano:true, _id: false }).limit(10);
+db.voos.find({ "empresa.nome": "GOL", ano: 2017 },
+    {
+        vooId: true,
+        "empresa.nome": true,
+        "aeroportoOrigem.nome": true,
+        "aeroportoDestino.nome": true,
+        mes: true,
+        ano: true,
+        _id: false
+    }).limit(10);
